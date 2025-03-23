245b8aa88b677bfc285ac5f361b64c3874cee346
kakarottoooo <gzw13979725269@gmail.com>
2025-03-23 17:25:37 -0500

    update CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://resplendent-moxie-a90d71.netlify.app",
     credentials: true,
   })
 );
